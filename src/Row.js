88a465bb7973f3476c9b9d6104c4b2ee76e815c9
Jorge <jorgediazok@gmail.com>
2020-10-07 03:37:43 -0300

    styling posters

--- src/Row.js
+++ src/Row.js
@@ -2,18 +2,33 @@ import React, { useState, useEffect } from 'react';
 import axios from './axios';
 import './Row.css';
 
+const base_url = 'https://image.tmdb.org/t/p/original/';
+
 function Row({ title, fetchUrl }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {}
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
     fetchData();
-  }, []);
+  }, [fetchUrl]);
+  console.log(movies);
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
-      <div>{fetchUrl}</div>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+            className="row__poster"
+          />
+        ))}
+      </div>
     </div>
   );
 }
